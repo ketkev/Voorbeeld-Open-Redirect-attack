7e30e74e11402d7d4c7ae9cff8a8aab485af80b5
ketkev <kdjoosten@gmail.com>
2020-02-28 13:32:13 +0100

    Simplify Protected Form

--- OpenRedirect/Pages/ProtectedForm.cshtml.cs
+++ OpenRedirect/Pages/ProtectedForm.cshtml.cs
@@ -10,30 +10,15 @@ namespace OpenRedirect
 {
     public class ProtectedFormModel : PageModel
     {
-        private readonly LoginModelDBContext _loginModel;
-        public ProtectedFormModel(LoginModelDBContext context)
-        {
-            _loginModel = context;
-        }
+
         public IActionResult OnGet()
         {
             return Page();
         }
 
-        [BindProperty]
-        public LoginModel LoginModel { get; set; }
-
-        public async Task<IActionResult> OnPostAsync()
+        public IActionResult OnPost()
         {
             string redirectURL = HttpContext.Request.Query["returnURL"].ToString();
-
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _loginModel.LoginModels.Add(LoginModel);
-            await _loginModel.SaveChangesAsync();
 
             if (Url.IsLocalUrl(redirectURL))
             {
