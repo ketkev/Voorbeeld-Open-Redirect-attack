804a7be7f5598fa5c815a1322ab73bf1bbbbf774
ketkev <kdjoosten@gmail.com>
2020-02-28 13:34:39 +0100

    Remove unused models

--- OpenRedirect/Startup.cs
+++ OpenRedirect/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenRedirect.Models;
 
 namespace OpenRedirect
 {
@@ -25,8 +19,6 @@ namespace OpenRedirect
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<LoginModelDBContext>(options =>
-                      options.UseInMemoryDatabase("name"));
             services.AddRazorPages();
         }
 
