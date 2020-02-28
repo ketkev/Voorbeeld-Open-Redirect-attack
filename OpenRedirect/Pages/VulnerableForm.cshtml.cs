fe87fd23d19be6846040e45d6c861ff6a8303a12
ketkev <kdjoosten@gmail.com>
2020-02-28 13:31:01 +0100

    Simplify Vulnerable Form Page

--- OpenRedirect/Pages/VulnerableForm.cshtml.cs
+++ OpenRedirect/Pages/VulnerableForm.cshtml.cs
@@ -10,31 +10,15 @@ namespace OpenRedirect
 {
     public class VulnerableFormModel : PageModel
     {
-        private readonly LoginModelDBContext _loginModel;
-        public VulnerableFormModel(LoginModelDBContext context)
-        {
-            _loginModel = context;
-        }
         public IActionResult OnGet()
         {
             return Page();
         }
 
-        [BindProperty]
-        public LoginModel LoginModel { get; set; }
-
-        public async Task<IActionResult> OnPostAsync()
+        public IActionResult OnPost()
         {
             string redirectURL = HttpContext.Request.Query["returnURL"].ToString();
             redirectURL = (redirectURL == "" ? "./Index" : redirectURL);
-
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _loginModel.LoginModels.Add(LoginModel);
-            await _loginModel.SaveChangesAsync();
 
             return Redirect(redirectURL);
         }
